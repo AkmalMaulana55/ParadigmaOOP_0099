4edd7d75f306adc4f532e33ffe80fe44cb3a0c2c
niel55-stack <vylingsniel55@gmail.com>
2026-04-20 18:25:44 +0700

    membuat method

--- tugasParadigmaOOP.cpp
+++ tugasParadigmaOOP.cpp
@@ -8,4 +8,13 @@ public:
     int jumlah;
     string kategori;
     string tanggalProduksi;
+
+    // Method untuk menampilkan data barang
+    void tampilkanData() {
+        cout << "Nama Barang       : " << nama << endl;
+        cout << "Jumlah            : " << jumlah << endl;
+        cout << "Kategori          : " << kategori << endl;
+        cout << "Tanggal Produksi  : " << tanggalProduksi << endl;
+        cout << "-----------------------------" << endl;
+    }
 };
