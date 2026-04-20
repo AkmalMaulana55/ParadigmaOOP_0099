2d4fc06bf3c78a0701af288e7f39902de5913a63
niel55-stack <vylingsniel55@gmail.com>
2026-04-20 18:25:27 +0700

    membuat class

--- tugasParadigmaOOP.cpp
+++ tugasParadigmaOOP.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// Membuat class Barang
+class Barang {
+public:
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalProduksi;
+};
